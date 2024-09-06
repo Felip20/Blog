8d6594b61b6c67e83e75491f02d2cb0b2fdff825
Wathone Khant <wathone@wathones-air.mynetworksettings.com>
2024-09-05 21:09:53 -0400

    first blog system

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Restart Blog</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+        <article>
+            <h1><a href="/blog">First Blog</a></h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            eum dolores voluptate velit odio nemo optio explicabo temporibus. 
+            Perspiciatis laborum repellendus omnis eligendi dignissimos vero 
+            vel ratione provident in eos.
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/blog">Second Blog</a></h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            eum dolores voluptate velit odio nemo optio explicabo temporibus. 
+            Perspiciatis laborum repellendus omnis eligendi dignissimos vero 
+            vel ratione provident in eos.
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/blog">Third Blog</a></h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            eum dolores voluptate velit odio nemo optio explicabo temporibus. 
+            Perspiciatis laborum repellendus omnis eligendi dignissimos vero 
+            vel ratione provident in eos.
+            </p>
+        </article>
+        
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blogs');
+});
+
+Route:: get('/blog', function () {
+    $filename='first-blog';
+    $path=__DIR__. "/../resources/blogs/$filename.html";
+    $blog=file_get_contents($path);
+    return view('blog', [
+        'blog'=> $blog
+    ]);
 });
