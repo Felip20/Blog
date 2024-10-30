38f3f066858fb6383fb16c02fdafab48594f03e9
Wathone Khant <wathone@wathones-air.mynetworksettings.com>
2024-10-29 23:02:06 -0400

    yield and layout

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Restart Blog</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-        <h1><?= $blog->title; ?></h1>
-        <p><?= $blog->body;?></p>
-        <a href="/">Back</a>
 
-</body>
-</html>
+@extends('layout')
+
+@section('title')
+    <title>{{$blog->title}}</title>
+@endsection
+
+@section('content')
+    <h1>{{$blog->title}}</h1>
+    <p>{{$blog->body}}</p>
+    <a href="/">Back</a>
+@endsection
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,23 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Restart Blog</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-    <?php foreach($blogs as $blog):?>
-    <h1><a href="blogs/<?= $blog->slug;?>"><?= $blog->title;?></a></h1>
+@extends('layout')
+
+@section('title')
+    <title>All Blogs</title>
+@endsection
+
+@section('content')
+    @foreach($blogs as $blog)
+    <h1><a href="blogs/{{$blog->slug}}">
+        {{$blog->title}}</a>
+    </h1>
     <div>
         <p>
-            Tested at - <?= $blog->date;?>
+            Tested at - {{$blog->date}}
         </p>
         <p>
-            <?= $blog->intro;?>
+            {{$blog->intro}}
         </p>
     </div>
-    <?php endforeach; ?>
-</body>
-</html>
+    @endforeach
+@endsection
+
