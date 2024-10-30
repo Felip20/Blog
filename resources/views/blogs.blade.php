ef18590eb88a3da8ac2a9a826025948ecc1b7e3f
Wathone Khant <wathone@wathones-air.mynetworksettings.com>
2024-10-29 23:26:49 -0400

    component

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,22 +1,20 @@
-@extends('layout')
+<x-layout>
+    <x-slot name="title">
+        <title>All blogs</title>
+    </x-slot>
+        @foreach($blogs as $blog)
+        <h1><a href="blogs/{{$blog->slug}}">
+            {{$blog->title}}</a>
+        </h1>
+        <div>
+            <p>
+                Tested at - {{$blog->date}}
+            </p>
+            <p>
+                {{$blog->intro}}
+            </p>
+        </div>
+        @endforeach
+</x-layout>
 
-@section('title')
-    <title>All Blogs</title>
-@endsection
-
-@section('content')
-    @foreach($blogs as $blog)
-    <h1><a href="blogs/{{$blog->slug}}">
-        {{$blog->title}}</a>
-    </h1>
-    <div>
-        <p>
-            Tested at - {{$blog->date}}
-        </p>
-        <p>
-            {{$blog->intro}}
-        </p>
-    </div>
-    @endforeach
-@endsection
 
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,15 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @yield('title')
+    {{$title}}
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <nav>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Contact</a></li>
-    </nav>
-    @yield('content')
+    <x-navigation/>
+    {{$slot}}
 </body>
 </html>
