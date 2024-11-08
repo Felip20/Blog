9254e118680dc6a0c3e75d129be23a63bbd4ce71
Wathone Khant <wathone@wathones-air.mynetworksettings.com>
2024-11-07 22:26:33 -0500

    belongsTo relationship

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -3,9 +3,12 @@
         <title>All blogs</title>
     </x-slot>
         @foreach($blogs as $blog)
-        <h1><a href="blogs/{{$blog->id}}">
+        <h1><a href="blogs/{{$blog->slug}}">
             {{$blog->title}}</a>
         </h1>
+        <p>
+            <a href="">{{$blog->category->name}}</a>
+        </p>
         <div>
             <p>
                 Tested at - {{$blog->created_at->diffForHumans()}}
