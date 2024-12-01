ca2d21edf054fd954d6844734dbe50c38ab32a16
Wathone Khant <wathone@wathones-air.mynetworksettings.com>
2024-11-30 22:09:10 -0500

    Create register form and CSRF

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Models\Blog;
-use App\Models\Category;
-use App\Models\User;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
@@ -11,4 +9,5 @@ use Illuminate\Support\Facades\DB;
 Route::get('/', [BlogController::class,'index']);
 Route:: get('/blogs/{blog:slug}', [BlogController::class,'show']);
 
-
+Route::get('/register', [AuthController::class,'create']);
+Route::post('/register',[AuthController::class,'store']);
